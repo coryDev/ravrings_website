be9915227d74dbf123cbe6db31feaeb9a0df5f32
Cory Hoffman <cory13dev@outlook.com>
2019-07-22 20:36:32 -0400

    bootstrap carousel added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,17 +20,51 @@
                     <div class="col px-0">
                     <div class="row">
                         <div class="col-lg-6">
-                        <h1 class="display-3 text-white">Compare websites & shop</h1>
-                        <p class="lead text-white">customer reviews & experience with. get friends advice - Ratings and purchase history with</p>
-                        <div class="btn-wrapper">
-                            <a href="" class="btn btn-info btn-icon mb-3 mb-sm-0">
-                            <span class="btn-inner--text">Merchants</span>
-                            </a>
-                            <a href="" class="btn btn-white btn-icon mb-3 mb-sm-0">
-                            <span class="btn-inner--text">Reviews</span>
-                            </a>
+                            <h1 class="display-3 text-white">Compare websites & shop</h1>
+                            <p class="lead text-white">customer reviews & experience with. get friends advice - Ratings and purchase history with</p>
+                            <div class="btn-wrapper">
+                                <a href="" class="btn btn-info btn-icon mb-3 mb-sm-0">
+                                <span class="btn-inner--text">Merchants</span>
+                                </a>
+                                <a href="" class="btn btn-white btn-icon mb-3 mb-sm-0">
+                                <span class="btn-inner--text">Reviews</span>
+                                </a>
+                            </div>
                         </div>
-                        </div>
+                        <div class="col-lg-5 mb-lg-auto">
+                            <div class="rounded shadow-lg overflow-hidden transform-perspective-right">
+                              <div id="carousel_example2" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                  <li data-target="#carousel_example2" data-slide-to="0" class="active"></li>
+                                  <li data-target="#carousel_example2" data-slide-to="1" class=""></li>
+                                  <li data-target="#carousel_example2" data-slide-to="2" class=""></li>
+                                  <li data-target="#carousel_example2" data-slide-to="3" class=""></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                    <img class="img-fluid" src="{{ asset('img/theme/team-1-800x800.jpg') }}" alt="First slide">
+                                  </div>
+                                  <div class="carousel-item">
+                                      <img class="img-fluid" src="{{ asset('img/theme/team-2-800x800.jpg') }}" alt="Middle slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img class="img-fluid" src="{{ asset('img/theme/team-3-800x800.jpg') }}" alt="Middle slide">
+                                    </div>
+                                  <div class="carousel-item">
+                                    <img class="img-fluid" src="{{ asset('img/theme/team-4-800x800.jpg') }}" alt="Second slide">
+                                  </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carousel_example2" role="button" data-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carousel_example2" role="button" data-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="sr-only">Next</span>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
                     </div>
                     </div>
                 </div>
@@ -160,6 +194,65 @@
             </div>
             </div>
         </section>
+        <section class="section section-shaped">
+            <div class="shape shape-style-1 shape-default">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div class="container py-md">
+              <div class="row justify-content-between align-items-center">
+                <div class="col-lg-5 mb-5 mb-lg-0">
+                  <h1 class="text-white font-weight-light">Bootstrap carousel</h1>
+                  <p class="lead text-white mt-4">Argon Design System comes with four pre-built pages to help you get started faster. You can change the text and images and you're good to go.</p>
+                  <a href="" class="btn btn-white mt-4">See all components</a>
+                </div>
+                <div class="col-lg-6 mb-lg-auto">
+                  <div class="rounded shadow-lg overflow-hidden transform-perspective-right">
+                    <div id="carousel_example" class="carousel slide" data-ride="carousel">
+                      <ol class="carousel-indicators">
+                        <li data-target="#carousel_example" data-slide-to="0" class=""></li>
+                        <li data-target="#carousel_example" data-slide-to="1" class=""></li>
+                        <li data-target="#carousel_example" data-slide-to="2" class=""></li>
+                        <li data-target="#carousel_example" data-slide-to="3" class="active"></li>
+                      </ol>
+                      <div class="carousel-inner">
+                        <div class="carousel-item">
+                          <img class="img-fluid" src="{{ asset('img/theme/team-1-800x800.jpg') }}" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="img-fluid" src="{{ asset('img/theme/team-2-800x800.jpg') }}" alt="Middle slide">
+                          </div>
+                          <div class="carousel-item">
+                            <img class="img-fluid" src="{{ asset('img/theme/team-3-800x800.jpg') }}" alt="Middle slide">
+                          </div>
+                        <div class="carousel-item active">
+                          <img class="img-fluid" src="{{ asset('img/theme/team-4-800x800.jpg') }}" alt="Second slide">
+                        </div>
+                      </div>
+                      <a class="carousel-control-prev" href="#carousel_example" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carousel_example" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- SVG separator -->
+            <div class="separator separator-bottom separator-skew">
+              <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
+              </svg>
+            </div>
+          </section>
         <section class="section bg-secondary">
             <div class="container">
                 <div class="row row-grid align-items-center">
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,19 +7,19 @@ namespace Composer\Autoload;
 class ComposerStaticInite992c8fc0cf98ad5292f02efaf8be76b
 {
     public static $files = array (
-        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '1d1b89d124cc9cb8219922c9d5569199' => __DIR__ . '/..' . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
-        '538ca81a9a966a6716601ecf48f4eaef' => __DIR__ . '/..' . '/opis/closure/functions.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        '538ca81a9a966a6716601ecf48f4eaef' => __DIR__ . '/..' . '/opis/closure/functions.php',
+        '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
     );
@@ -147,8 +147,8 @@ class ComposerStaticInite992c8fc0cf98ad5292f02efaf8be76b
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'XdgBaseDir\\' => 
         array (
